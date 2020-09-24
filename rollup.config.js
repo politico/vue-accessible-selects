433008e776c16c2824f4e806153b3400fe8cc036
Jack Koppa <hello@jackkoppa.com>
2020-09-24 11:07:21 -0400

    refactor (#15): Simplify building of SCSS to use rollup, & imports to be able to import via index.scss file See https://github.com/webpack-contrib/sass-loader/issues/535\#issuecomment-394767296

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from 'rollup-plugin-typescript2'
 import VuePlugin from 'rollup-plugin-vue'
+import copy from 'rollup-plugin-copy'
 
 import pkg from './package.json'
 
@@ -16,6 +17,12 @@ export default {
         typescript(),
         VuePlugin({
             css: false
+        }),
+        copy({
+            // For now, we only support usage of the styling as SCSS mixins, and thus only need to copy over the SCSS without compilation
+            // In the future, we could easily export compiled CSS by request, & perhaps allow for CSS vars replacing SCSS vars for customization in that case
+            targets: [{ src: 'src/**/*.scss', dest: 'dist' }],
+            flatten: false
         })
     ],
 };
