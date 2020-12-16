f48dbd590a4b6b020ec3410cbd6f1803c2e6bed0
Jack Koppa <hello@jackkoppa.com>
2020-12-16 03:43:54 -0500

    test (#58): Add most basic, working specs for single & multi These initial specs do the bare minimum: test that options display, and that when clicked, the options are correctly selected (displayed correctly, and update the v-model value) The main next TODOs for new specs will be a11y-related: checking that aria labels are handled correctly, for example And test keyboard behavior

---
+++ specs/SelectSingle.spec.ts
@@ -0,0 +1,55 @@
+import { mount, Wrapper } from '@vue/test-utils'
+
+import SelectSingle from '../src/SelectSingle.vue'
+
+describe('SelectSingle', () => {
+	let wrapper: Wrapper<Vue>
+
+	describe('given simple options', () => {
+		beforeEach(() => {
+			// TODO: Share sample data between spec files & Storybook stories
+			const options = [
+				{ label: 'Item One', value: 'one' },
+				{ label: 'Item Two', value: 'two' },
+				{ label: 'Item Three', value: 'three' }
+			]
+
+			wrapper = mount({
+				data() { 
+					return { selectedOption: {}, options }},
+				template: '<div><SelectSingle :options="options" v-model="selectedOption" label="Sample SelectSingle" /></div>',
+				components: { SelectSingle }
+			})
+		})
+
+		it('contains all 3 given options in the dropdown', () => {
+			const listboxText = wrapper.find("div[role=listbox]").text()
+
+			expect(listboxText).toContain('Item One')
+			expect(listboxText).toContain('Item Two')
+			expect(listboxText).toContain('Item Three')
+		})
+
+		describe('when the second option is clicked', () => {
+			beforeEach(async () => {
+				const listbox = wrapper.find("div[role=listbox]")
+				await listbox.findAll('div[role=option]').at(1).trigger('click')
+			})
+
+			it('sets the new displayed value of the select appropriately', async () => {
+				expect(wrapper.find('div[role=combobox]').text()).toContain('Item Two')
+			})
+
+			it('emits the full selected item exactly once', async () => {
+				const selectSingle = wrapper.findComponent(SelectSingle)
+
+				expect(selectSingle.emitted().select.length).toEqual(1)
+				expect(selectSingle.emitted().select[0]).toEqual([{ label: 'Item Two', value: 'two' }])
+			})
+
+			it('sets the parent component data property correctly, given v-model usage', async () => {
+				expect(wrapper.vm['selectedOption']).toEqual({ label: 'Item Two', value: 'two' })
+			})
+		})
+	})
+})
