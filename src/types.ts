5216df11cc2a4b7b8201c168996a79e26f037923
Jack Koppa <hello@jackkoppa.com>
2021-02-21 12:53:23 -0500

    refactor: Allow more flexible SelectOption def
    While we still want to default to the { label: string, value: string } shape for the SelectOption[] array passed into our components,
    (it's somewhat selfish for the Politico team - that shape is the most common for our usage & API responses -
    but simply having a default of some kind is intended to help all users by saying 'if you use this default shape, you can provide many fewer props,
    and the components will still just work')
    we now open up the type definition to essentially say "any object shape is OK, you're just going to need to set some more props on the component
    to make label display & select behavior work"

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,19 @@
+/**
+ * @summary
+ * The shape of a single option provided to `SelectSingle` or `SelectMulti`
+ *  
+ * @default
+ * { label: string, value: string }
+ * 
+ * @description
+ * If your options *do not* match the default { label, value } shape, 
+ * some additional props
+ * (e.g. `labelField` & `uniqueIdField`)
+ * will need to be set on the `SelectSingle` / `SelectMulti` components
+ * in order to ensure proper display & selecting/deselecting behavior
+ */
 export interface SelectOption {
-	label: string
-	value: string | string[]
+	label?: string
+	value?: string
+	[key: string]: any
 }
