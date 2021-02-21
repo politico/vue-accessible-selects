d116952ce0ec39c13b5abed15f96b9a370c4dc34
Jack Koppa <hello@jackkoppa.com>
2021-02-21 16:59:45 -0500

    test (#78): Add specs for compare-by-value
    Add specs (including two that currently fail, because of the bug/feature request in #78)
    for confirming that, when the list of `options` is updated in the parent component,
    we are still able to accurately compare our current `selectedOptions` array to the new `options` list,
    to determine which should be displayed as selected.
    
    Next steps are to actually fix the code in SelectMulti so the specs can pass
    
    Additional work: refactoring the specs file so that there can be a bit of easily-repeatable specs,
    in different component initialization setups
    (primary example so far is that we can extract the 'mount' method,
    so each type of spec only has to define the new SelectMulti template string it would like to use,
    and grouping all the specs related to confirming behavior when selecting options, **and** passing in a new reference to the list of options)
    
    Note: we've still tried to follow https://github.com/mawrkus/js-unit-testing-guide as much as possible
    Namely, while there is some repeated code extracted to functions, we are still NOT:
    * involving logic in our specs - we are NOT using if/else or loops; only using callbacks when we need more than simple extraction of repeated code
    * using comments to document the specs - our `describe` and `it` blocks are fairly verbose, because we are preferring long spec descriptions,
    to requiring devs to read code comments in order to understand the purpose of each spec

--- specs/SelectMulti.spec.ts
+++ specs/SelectMulti.spec.ts
@@ -8,20 +8,86 @@ import {
 	optionsWithCustomLabelAndUniqueIdFields
 } from './sampleData'
 
+function duplicateOptionsAsNewArrayInstance(options: SelectOption[]) {
+	return [...options.map(option => ({ ...option }))]
+}
+
 describe('SelectMulti', () => {
 	let wrapper: Wrapper<Vue>
 	let options: SelectOption[]
+
+	function mountSelectMulti(selectMultiTemplate) {
+		return mount({
+			data() { return { selectedOptions: [], options }},
+			template: `<div>${selectMultiTemplate}</div>`,
+			components: { SelectMulti },
+			methods: { 
+				setOptions(newOptions) { (this as any).options = newOptions }
+			}
+		})
+	}
+
+	function testBehaviorForSelectingMultipleOptions(originalOptions: SelectOption[], testBehaviorForEventEmitsAndVModel?: () => void) {
+		describe('when the second option is clicked & then the third option is clicked', () => {
+			beforeEach(async () => {
+				const listbox = wrapper.find("div[role=listbox]")
+				const divOptions = listbox.findAll('div[role=option]')
+				await divOptions.at(1).trigger('click')
+				await divOptions.at(2).trigger('click')
+			})
+
+			it('displays the correctly selected pills', async () => {
+				const selectedOptionsText = wrapper.find('.selected-options').text()
+
+				expect(selectedOptionsText).toContain('Item Two')
+				expect(selectedOptionsText).toContain('Item Three')
+			})
+
+			it('marks the options in the dropdown as selected, with the correct class & aria attribute', async () => {
+				const listbox = wrapper.find("div[role=listbox]")
+				const selectedDivOptions = listbox.findAll('div.option-selected[role=option][aria-selected=true]')
+
+				expect(selectedDivOptions.at(0).text()).toEqual('Item Two')
+				expect(selectedDivOptions.at(1).text()).toEqual('Item Three')
+			})
+
+			testBehaviorForEventEmitsAndVModel?.()
+
+			describe('(testing finding items by value, rather than reference) ' + 
+				'then something occurs in the parent such that the options array is updated to be a new instance (of the same original array)', () => {
+				beforeEach(() => {
+					wrapper.vm['setOptions'](duplicateOptionsAsNewArrayInstance(originalOptions))
+				})
+
+				it('continues to display the correctly selected pills', () => {
+					const selectedOptionsText = wrapper.find('.selected-options').text()
+					expect(selectedOptionsText).toContain('Item Two')
+					expect(selectedOptionsText).toContain('Item Three')
+				})
+
+				it('still marks the options in the dropdown as selected, with the correct class & aria attribute', () => {
+					const listbox = wrapper.find("div[role=listbox]")
+					const selectedDivOptions = listbox.findAll('div.option-selected[role=option][aria-selected=true]')
+
+					expect(selectedDivOptions.at(0).text()).toEqual('Item Two')
+					expect(selectedDivOptions.at(1).text()).toEqual('Item Three')
+				})
+			})
+		})
+	}
 
 	describe('given simple options', () => {
 		beforeEach(() => options = simpleOptions )
 
 		describe('and a simple SelectMulti initialized with options, v-model, and a label', () => {
 			beforeEach(() => {
-				wrapper = mount({
-					data() { return { selectedOptions: [], options }},
-					template: '<div><SelectMulti :options="options" v-model="selectedOptions" label="Sample SelectMulti" /></div>',
-					components: { SelectMulti }
-				})
+				wrapper = mountSelectMulti(`
+					<SelectMulti 
+						:options="options"
+						v-model="selectedOptions"
+						label="Sample SelectSingle"
+					/>
+				`)
 			})
 
 			it('contains all 3 given options in the dropdown', () => {
@@ -32,35 +98,23 @@ describe('SelectMulti', () => {
 				expect(listboxText).toContain('Item Three')
 			})
 	
-			describe('when the second option is clicked & then the third option is clicked', () => {
-				beforeEach(async () => {
-					const listbox = wrapper.find("div[role=listbox]")
-					await listbox.findAll('div[role=option]').at(1).trigger('click')
-					await listbox.findAll('div[role=option]').at(2).trigger('click')
-				})
-	
-				it('adds those options to the list of selected options', async () => {
-					const selectedOptionsText = wrapper.find('.selected-options').text()
-					expect(selectedOptionsText).toContain('Item Two')
-					expect(selectedOptionsText).toContain('Item Three')
-				})
-	
+			testBehaviorForSelectingMultipleOptions(simpleOptions, () => {
 				it('emits the full selected item each time an item is selected, using the `select` event', async () => {
 					const selectMulti = wrapper.findComponent(SelectMulti)
-	
+
 					expect(selectMulti.emitted().select.length).toEqual(2)
 					expect(selectMulti.emitted().select[0]).toEqual([{ label: 'Item Two', value: 'two' }])
 					expect(selectMulti.emitted().select[1]).toEqual([{ label: 'Item Three', value: 'three' }])
 				})
-	
+
 				it('emits the current list of all selected options each time the selected options are updated, using the `change` event to work w/ v-model', async () => {
 					const selectMulti = wrapper.findComponent(SelectMulti)
-	
+
 					expect(selectMulti.emitted().change.length).toEqual(2)
 					expect(selectMulti.emitted().change[0]).toEqual([[{ label: 'Item Two', value: 'two' }]])
 					expect(selectMulti.emitted().change[1]).toEqual([[{ label: 'Item Two', value: 'two' },{ label: 'Item Three', value: 'three' }]])
 				})
-	
+
 				it('sets the parent component data property correctly, given v-model usage', async () => {
 					expect(wrapper.vm['selectedOptions']).toEqual([{ label: 'Item Two', value: 'two' },{ label: 'Item Three', value: 'three' }])
 				})
@@ -73,21 +127,15 @@ describe('SelectMulti', () => {
 		
 		describe('with a SelectMulti initialized with labelField & uniqueIdField props', () => {
 			beforeEach(() => {
-				wrapper = mount({
-					data() { return { selectedOptions: [], options }},
-					template: `
-						<div>
-							<SelectMulti 
-								:options="options"
-								v-model="selectedOptions"
-								label="Sample SelectSingle"
-								labelField="customLabel"
-								uniqueIdField="customUniqueId"
-							/>
-						</div>
-					`,
-					components: { SelectMulti }
-				})
+				wrapper = mountSelectMulti(`
+					<SelectMulti 
+						:options="options"
+						v-model="selectedOptions"
+						label="Sample SelectSingle"
+						labelField="customLabel"
+						uniqueIdField="customUniqueId"
+					/>
+				`)
 			})
 
 			it('displays the correct label for each option in the dropdown', () => {
@@ -99,35 +147,23 @@ describe('SelectMulti', () => {
 				expect(optionDivs.at(2).text()).toEqual('Item Three')
 			})
 
-			describe('when the second option is clicked & then the third option is clicked', () => {
-				beforeEach(async () => {
-					const listbox = wrapper.find("div[role=listbox]")
-					await listbox.findAll('div[role=option]').at(1).trigger('click')
-					await listbox.findAll('div[role=option]').at(2).trigger('click')
-				})
-
-				it('adds those options to the list of selected options', async () => {
-					const selectedOptionsText = wrapper.find('.selected-options').text()
-					expect(selectedOptionsText).toContain('Item Two')
-					expect(selectedOptionsText).toContain('Item Three')
-				})
-	
+			testBehaviorForSelectingMultipleOptions(optionsWithCustomLabelAndUniqueIdFields, () => {
 				it('emits the full selected item each time an item is selected, using the `select` event', async () => {
 					const selectMulti = wrapper.findComponent(SelectMulti)
-	
+
 					expect(selectMulti.emitted().select.length).toEqual(2)
 					expect(selectMulti.emitted().select[0]).toEqual([{ customLabel: 'Item Two', customUniqueId: 'two' }])
 					expect(selectMulti.emitted().select[1]).toEqual([{ customLabel: 'Item Three', customUniqueId: 'three' }])
 				})
-	
+
 				it('emits the current list of all selected options each time the selected options are updated, using the `change` event to work w/ v-model', async () => {
 					const selectMulti = wrapper.findComponent(SelectMulti)
-	
+
 					expect(selectMulti.emitted().change.length).toEqual(2)
 					expect(selectMulti.emitted().change[0]).toEqual([[{ customLabel: 'Item Two', customUniqueId: 'two' }]])
 					expect(selectMulti.emitted().change[1]).toEqual([[{ customLabel: 'Item Two', customUniqueId: 'two' },{ customLabel: 'Item Three', customUniqueId: 'three' }]])
 				})
-	
+
 				it('sets the parent component data property correctly, given v-model usage', async () => {
 					expect(wrapper.vm['selectedOptions']).toEqual([{ customLabel: 'Item Two', customUniqueId: 'two' },{ customLabel: 'Item Three', customUniqueId: 'three' }])
 				})
