1856f7bd0e81fa7525ccdd49b570230f24c7827a
Tilovon Crite <tcrite@politico.com>
2023-07-31 08:39:21 -0400

    copying styles up one level

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import copy from 'ro'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 import pkg from './package.json'
@@ -12,7 +13,7 @@ export default defineConfig({
     viteStaticCopy({
       // For now, we only support usage of the styling as SCSS mixins, and thus only need to copy over the SCSS without compilation
       // In the future, we could easily export compiled CSS by request, & perhaps allow for CSS vars replacing SCSS vars for customization in that case
-      targets: [{ src: './src/styles', dest: './' }]
+      targets: [{ src: './src/styles', dest: '../' }]
     })
   ],
   optimizeDeps: {
