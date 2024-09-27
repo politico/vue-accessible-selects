9c94615a7d97960893fce12a48aee3341c0d1943
Anna Shpinkova <annashp@nitka.com>
2024-09-27 12:40:12 +0100

    AIQ-5472: Add iconIsClickable props

--- docs/SelectMulti.stories.ts
+++ docs/SelectMulti.stories.ts
@@ -23,6 +23,7 @@ const Template = (args, { argTypes }) => ({
 			:labelIsVisible="labelIsVisible"
 			:placeholder="placeholder"
 			:disabled="disabled"
+			:iconIsClickable="iconIsClickable"
 			:displayPillsBelowInput="displayPillsBelowInput"
 			:noResultsMessage="noResultsMessage"
 			:uniqueIdField="uniqueIdField"
@@ -37,6 +38,7 @@ Primary.args = {
 	labelField: 'label',
 	labelIsVisible: true,
 	loading: false,
+	iconIsClickable: true,
 	placeholder: 'Select Options',
 	disabled: false,
 	displayPillsBelowInput: false,
